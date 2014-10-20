08db8a3f5bab9a49cef426537eed93b4f042ace4
Jason Sigal <jason.sigal@gmail.com>
2014-10-20 19:46:13 -0400

    keystroke playback

--- js/keyLogger.js
+++ js/keyLogger.js
@@ -2,12 +2,40 @@
   generate json file with timestamp, 
  */
 
-var keyLog = {}
+var keyLog = [];
+var takes = [];
 
 function logKeyDown(keyCode, note) {
   console.log('key down!' + keyCode + ', ' + note);
+  keyLog.push([Tone.context.currentTime, note, 'triggerAttack']);
 }
 
 function logKeyUp(keyCode, note) {
   console.log('key up!' + keyCode + ', ' + note);
+  keyLog.push([Tone.context.currentTime, note, 'triggerRelease']);
+}
+
+
+function saveTake() {
+  // save(keyLog, 'keylog.json');
+  var currentTake = keyLog;
+  takes.push(currentTake);
+
+  // reset keylog
+  keyLog = [];
+
+  // startTime is the time of the first note in the take
+  var startTime = currentTake[0][0];
+
+  for (var i in takes[takes.length - 1]) {
+    // schedule notes
+    if (currentTake[i][2] === 'triggerAttack') {
+      console.log('saving attack');
+      mySynth.triggerAttack( mySynth.midiToNote(currentTake[i][1]), mySynth.now() + currentTake[i][0] - startTime );
+    } else if (currentTake[i][2] === 'triggerRelease') {
+      console.log('saving release');
+      mySynth.triggerRelease( mySynth.midiToNote(currentTake[i][1]), mySynth.now() + currentTake[i][0] - startTime );
+    }
+  }
+  console.log(startTime);
 }
